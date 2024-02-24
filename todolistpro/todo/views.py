e36d7fd5a42d70e3c1cc5d245d4a182ede2af303
Sunidhi Sharma <sunidhisharma3002@gmail.com>
2024-02-24 21:46:11 +0545

    Second commit

--- todolistpro/todo/views.py
+++ todolistpro/todo/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from . models import Task
 
-def home(request):
-    return HttpResponse("Working")
+class TaskList(ListView):
+    model = Task
+    context_object_name='task'
+    
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name='task'
+    template_name = 'todo/task.html'
