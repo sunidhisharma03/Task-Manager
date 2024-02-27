c0507a94f7102dc2fa3349f9f133c9349763760c
Sunidhi Sharma <sunidhisharma3002@gmail.com>
2024-02-27 22:09:55 +0545

    Update View

--- todolistpro/todo/views.py
+++ todolistpro/todo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Task
 
 class TaskList(ListView):
@@ -12,3 +14,15 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name='task'
     template_name = 'todo/task.html'
+    
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    # fields = ['title','description']
+    success_url = reverse_lazy('task')
+    
+class TaskUpdate(UpdateView):
+    model = Task
+    fields= "__all__"
+    success_url = reverse_lazy('task')
+    
